f71a436aa4791f54b5478c1a3e343121e21562bf
Cory Freitag <Cory Freitag>
2017-02-08 14:24:47 -0800

    Adding list feature and fixing Sea of Clouds and Clicking list dropdown

--- ffmaterialbuddy.js
+++ ffmaterialbuddy.js
@@ -1,39 +1,83 @@
 class FFMaterialBuddy {
     constructor() {
         this.itemLocationMap = [];
-        this.sources = [
-            "gathering",
-            "enemy"
-        ];
-        this.promises = [];
+        this.searchList = [];
+        this.fullList = [];
         this.main = $("#MainTextArea");
         this.selector = $("#LocationSelector");
+        this.selectionBar = $("#SelectionBar");
         this.populateSelector();
         this.selector.keypress((e) => {
             if (e.keyCode == 13) {
                 (this.selector).autocomplete('close');
-                this.update();
+                this.update(e, null);
+            }
+        });
+        $(".addList").click((e) => {
+            var selection = ($("#LocationSelector")[0]).value.toLowerCase();
+            if (this.fullList.findIndex(item => item.toLowerCase() == selection) != -1
+                && this.searchList.indexOf(selection) == -1) {
+                this.searchList.push(selection);
+                this.updateSearchList();
+                this.update(null, null);
             }
         });
     }
     populateSelector() {
+        this.fullList = Locations.map.filter(map => map.map != "").map(map => map.map);
         (this.selector).autocomplete({
-            minLength: 2,
-            source: Locations.map.filter(map => map.map != "").map(map => map.map),
-            select: () => this.update(),
-            appendTo: "#AutoComplete"
+            minLength: 1,
+            source: this.fullList,
+            select: (e, ui) => this.update(e, ui),
+            appendTo: "#AutoComplete",
+            position: {
+                "my": "left top",
+                "at": "left bottom",
+                "of": this.selectionBar
+            }
         });
     }
-    update() {
-        var selected = ($("#LocationSelector")[0]).value.toLowerCase();
+    updateSearchList() {
+        var listElement = $("#SearchList");
+        listElement.empty();
+        for (var item of this.searchList) {
+            listElement.append($("<div/>", { class: "listElement" })
+                .append($("<span/>", { class: "listElementText", text: item }))
+                .append($("<span/>", { class: "listElementClose fa fa-remove", item: item })));
+        }
+        $(".listElementClose").click((e) => this.removeFromList(($(e)[0]).item));
+    }
+    removeFromList(item) {
+        var i = this.searchList.indexOf(item);
+        this.searchList.splice(i, 1);
+        this.updateSearchList();
+        this.update(null, null);
+    }
+    update(event, ui) {
+        var origEvent = event;
+        while (origEvent && origEvent.originalEvent !== undefined)
+            origEvent = origEvent.originalEvent;
+        var selected = "";
+        if (origEvent && origEvent.type == "click") {
+            selected = ui.item.value.toLowerCase();
+        }
+        else {
+            selected = ($("#LocationSelector")[0]).value.toLowerCase();
+        }
         if (selected == "")
             return;
         var index = Locations.map.findIndex((loc) => loc.map.toLowerCase() == selected);
         if (index == -1)
             return;
-        var ids = Locations.map[index].ids;
-        var itemsRaw = Final.results.filter((item) => ids.indexOf(item.id) != -1);
         this.main.empty();
+        var ids = new Set(Locations.map[index].ids);
+        for (var item of this.searchList) {
+            var searchindex = Locations.map.findIndex((loc) => loc.map.toLowerCase() == item);
+            $.each(Locations.map[searchindex].ids, (key, value) => {
+                ids.add(value);
+            });
+        }
+        var itemsRaw = Final.results.filter((item) => ids.has(item.id));
         var prettyItems = itemsRaw.map(item => this.formHTML(selected, item)).reduce((total, add) => total.append(add), this.main);
         $(".expandedBlock").css({ height: 0 });
         $(".enemyButton.enabled").text("Enemy +");
@@ -79,7 +123,7 @@ class FFMaterialBuddy {
         if (item.enemies != "") {
             var enemiesDiv = htmlblock.find(".enemies");
             for (var enemy of item.enemies) {
-                if (enemy.map && enemy.map.mapname.toLowerCase() == selected) {
+                if (enemy.map && (enemy.map.mapname.toLowerCase() == selected || this.searchList.indexOf(enemy.map.mapname.toLowerCase()) != -1)) {
                     enemiesDiv.append($("<div/>", { class: 'enemiesblock expand' })
                         .append($("<span/>", { class: "enemyName", text: enemy.name }))
                         .append($("<span/>", { class: "enemyLevel", text: (enemy.maxlevel == enemy.minlevel)
@@ -93,7 +137,7 @@ class FFMaterialBuddy {
             var gatheringDiv = htmlblock.find(".gathering");
             var gathering = item.gathering[0];
             for (var node of gathering.node) {
-                if (node.mapname && node.mapname.toLowerCase() == selected) {
+                if (node.mapname && (node.mapname.toLowerCase() == selected || this.searchList.indexOf(node.mapname.toLowerCase()) != -1)) {
                     var stars = Array.from(Array(gathering.stars).keys()).reduce((total, current) => total + "★", "");
                     var block = $("<div/>", { class: 'gatheringblock expand' }).appendTo(gatheringDiv);
                     block.append($("<span/>", { class: "nodeLevel", text: "Node: " + node.level }))
